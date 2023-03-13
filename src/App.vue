1bbcd36fe6745cc438cfd6f7f6ece2722190b3c2
Gümrah Sindar <99037179+gumrahsindar@users.noreply.github.com>
2023-03-13 13:53:27 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const rates = ref([1, 2, 3, 4, 5]);
           <img src="./assets/images/icon-star.svg" alt="" />
         </div>
         <div class="text">
-          <h2>How did we do?</h2>
+          <h1>How did we do?</h1>
           <p>
             Please let us know how we did with your support request. All
             feedback is appreciated to help us improve our offering!
